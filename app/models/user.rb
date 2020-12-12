5d4f3d961e0260046e0185f04c3b1a1e40cf235a
hiro0405 <crescent0712@gmail.com>
2020-12-12 21:05:38 +0900

    モデル単体テストコードの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true
-  validates :email, uniqueness: true
+  validates :email, presence: true
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: "Include both letters and numbers"
   validates :password, confirmation: true, length: { minimum: 6 }
---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname {Faker::Name.first_name}
+    email {Faker::Internet.free_email}
+    password {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    family_name {"米倉"}
+    first_name {"弘記"}
+    family_name_kana {"ヨネクラ"}
+    first_name_kana {"ヒロキ"}
+    birthday {"2020-07-12"}
+  end
+end
