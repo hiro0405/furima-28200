2459265bbffdedd3e0a7fd7f71cdd275e7efbf41
hiro0405 <crescent0712@gmail.com>
2020-12-13 19:54:55 +0900

    items テーブルの作成

---
+++ db/migrate/20201213104344_create_items.rb
@@ -0,0 +1,11 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string :name, null: false
+      t.text :description, null: false
+      t.integer :price, null: false
+      t.integer :user_id, null: false
+      t.timestamps
+    end
+  end
+end
