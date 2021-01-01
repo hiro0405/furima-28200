1230dd1e4ca98493ca771a95e928b52df625b010
hiro0405 <crescent0712@gmail.com>
2021-01-01 14:15:20 +0900

    商品情報をテーブルへ保存する実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
 
+  before_action :authenticate_user!, only: [:new]
+
   def index
     @items = Item.all
   end
@@ -20,7 +22,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :image, :description, :category_id, :status_id, :shipping_charge_id, :shipping_area_id, :estimated_shipping_date_id, :price)
+    params.require(:item).permit(:name, :image, :description, :category_id, :status_id, :shipping_charge_id, :shipping_area_id, :estimated_shipping_date_id, :price).merge(user_id: current_user.id)
   end
 
 end
